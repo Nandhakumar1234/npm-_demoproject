871135b41ca5f1693e25b03cf7e0d857412ec505
nandhakumar1234 <nandhakumarb99@gmail.com>
2023-01-20 04:34:55 +0000

    Second commit

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
-new sap.m.Input({
-
-    placeholder: 'Enter Filename',
-
-    required: true
-
-})
+var exports = module.exports = {};
+export function inputReuse () {
+    new sap.m.Input({
+        placeholder: 'Enter Filename',
+        required: true
+    })
+}
